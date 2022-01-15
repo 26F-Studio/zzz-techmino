ef6a40fd711b8fea4282344b1d55c98543e9791a
flaribbit <1149761294@qq.com>
2022-01-15 21:23:45 +0800

    emmylua

---
+++ zzz.lua
@@ -0,0 +1,13 @@
+---@class zzz
+local zzz={}
+
+---@return string
+function zzz.name() end
+
+---@param field boolean[]
+---@param hand string
+---@param hold string
+---@param canhold boolean
+---@param next string
+---@return string
+function zzz.run(field, hand, hold, canhold, next) end
