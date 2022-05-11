24bface3f035b1d20626c64da262240907ca600e
flaribbit <1149761294@qq.com>
2022-05-11 10:22:54 +0800

    返回错误信息

--- zzz_techmino.cpp
+++ zzz_techmino.cpp
@@ -1,10 +1,10 @@
 extern "C" {
 #include "lauxlib.h"
 }
-#include "tetris_core.h"
-#include "rule_toj.h"
 #include "ai_zzz.h"
+#include "rule_toj.h"
 #include "search_tspin.h"
+#include "tetris_core.h"
 
 static const int boardW = 10, boardH = 40;
 static m_tetris::TetrisEngine<rule_toj::TetrisRule, ai_zzz::TOJ, search_tspin::Search> tetris_ai;
@@ -13,12 +13,14 @@ static int combo_table[12] = {0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5};
 // run(field, hand, hold, canhold, next)
 static int run(lua_State *L) {
     if (!tetris_ai.prepare(boardW, boardH)) {
-        printf("tetris_ai prepare failed\n");
-        return 0;
+        lua_pushnil(L);
+        lua_pushstring(L, "tetris_ai prepare failed");
+        return 2;
     }
     if (!lua_istable(L, 1)) {
-        printf("invalid field\n");
-        return 0;
+        lua_pushnil(L);
+        lua_pushstring(L, "field should be a table");
+        return 2;
     }
     const char *hand = lua_tostring(L, 2);
     const char *hold = lua_tostring(L, 3);
@@ -50,7 +52,9 @@ static int run(lua_State *L) {
         lua_pushlstring(L, ai_path.data(), ai_path.size());
         return 1;
     }
-    return 0;
+    lua_pushnil(L);
+    lua_pushstring(L, "got nothing");
+    return 2;
 }
 
 static int name(lua_State *L) {
